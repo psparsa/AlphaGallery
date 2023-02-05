13bf66f569498af68b7418b5136a363fbe6aeaf4
PS-PARSA <57572461+psparsa@users.noreply.github.com>
2023-02-05 21:45:46 +0330

    color pallet added to tailwind config

--- source/client/tailwind.config.js
+++ source/client/tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   content: ['./src/**/*.{ts,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      snow: '#FFFAFA',
+      coralRed: '#FF4343',
+      begonia: '#FF7171',
+      taupeGray: '#878787',
+      chineseBack: '#14161A',
+      gunmetal: '#2C313A',
+    },
   },
   plugins: [],
 };
