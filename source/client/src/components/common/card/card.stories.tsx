f9925e41329b5479ae5ffba423e7ed7e59cf40dc
AmirabbasJ <amirabbasgamer2@gmail.com>
2023-02-08 19:04:18 +0330

    add props to card

--- source/client/src/components/common/card/card.stories.tsx
+++ source/client/src/components/common/card/card.stories.tsx
@@ -6,6 +6,15 @@ export default {
   component: Card,
 } as ComponentMeta<typeof Card>;
 
-const Template: ComponentStory<typeof Card> = () => <Card />;
+const Template: ComponentStory<typeof Card> = (arguments_) => (
+  <Card {...arguments_} />
+);
 
 export const Default = Template.bind({});
+Default.args = {
+  description: `Lorem ipsum dolor sit amet, consectetur adipiscing
+  elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+  aliqua.`,
+  title: 'Golden eye cat at garden',
+  categories: ['cats', 'cute', 'animals'],
+};
--- source/client/src/components/common/card/card.tsx
+++ source/client/src/components/common/card/card.tsx
@@ -3,25 +3,28 @@ import CatImage from '@/assets/cat.jpeg';
 import { Badge } from '../badge';
 import { Button } from '../button';
 
-export const Card = () => {
+interface Properties {
+  categories: string[];
+  description: string;
+  title: string;
+}
+
+export const Card = ({ title, description, categories }: Properties) => {
   return (
     <div className="flex w-97.5 flex-col overflow-hidden rounded-4xl bg-gunmetal">
       <Image alt="cat" className="h-87.5 w-full object-cover" src={CatImage} />
       <div className="flex flex-col gap-y-5 px-5 py-2">
         <div className="flex flex-col gap-y-2">
-          <h1 className="text-3xl">Golden eye cat at garden</h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </p>
+          <h1 className="text-3xl">{title}</h1>
+          <p>{description}</p>
         </div>
         <div className="flex flex-col">
           <div className="flex flex-row">
             <p className="font-normal">Categories:</p>
             <div className="flex flex-row gap-x-3 pl-3">
-              <Badge>cats</Badge>
-              <Badge>cute</Badge>
-              <Badge>animals</Badge>
+              {categories.map((cat) => (
+                <Badge key="cat">{cat}</Badge>
+              ))}
             </div>
           </div>
           <div className="py-3">
