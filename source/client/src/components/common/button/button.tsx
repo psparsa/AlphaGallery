93bbff18e2bf145f72db2d166006ce19168ac534
AmirabbasJ <amirabbasgamer2@gmail.com>
2023-02-08 12:17:06 +0330

    add button component

---
+++ source/client/src/components/common/button/button.tsx
@@ -0,0 +1,19 @@
+interface Props {
+  children: React.ReactNode;
+  variant?: 'dark' | 'red';
+}
+
+const colorVariants = {
+  red: 'bg-coralRed',
+  dark: 'bg-coralRed',
+} as const;
+
+export const Button = ({ children, variant = 'red' }: Props) => {
+  return (
+    <button
+      className={`w-full rounded-2xl ${colorVariants[variant]} p-2.5 font-normal hover:bg-opacity-80 active:bg-opacity-70`}
+    >
+      {children}
+    </button>
+  );
+};
