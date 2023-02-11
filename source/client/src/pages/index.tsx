2905a5eecaf6e6ad354409c3c524138b7946d6d0
PS-PARSA <57572461+psparsa@users.noreply.github.com>
2023-02-11 23:29:41 +0330

    adding page title to home page

--- source/client/src/pages/index.tsx
+++ source/client/src/pages/index.tsx
@@ -1,41 +1,47 @@
 import { Search, ScrollButton } from '@/components';
 import { Roboto } from '@next/font/google';
 import { twMerge } from 'tailwind-merge';
+import Head from 'next/head';
 
 const roboto = Roboto({ weight: ['300', '400'], subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <main
-      className={twMerge(
-        roboto.className,
-        'flex min-h-screen w-screen flex-col bg-chineseBlack font-light text-snow'
-      )}
-    >
-      <div
-        className="flex min-h-screen w-screen flex-col items-center
-        justify-center bg-chineseBlackVoid"
+    <>
+      <Head>
+        <title>AlphaGallery</title>
+      </Head>
+      <main
+        className={twMerge(
+          roboto.className,
+          'flex min-h-screen w-screen flex-col bg-chineseBlack font-light text-snow'
+        )}
       >
         <div
-          className="flex w-full flex-1 flex-col items-center
-            justify-center"
+          className="flex min-h-screen w-screen flex-col items-center
+        justify-center bg-chineseBlackVoid"
         >
-          <div className="text-center text-6xl font-light text-coralRed sm:text-7xl">
-            Alpha Gallery
+          <div
+            className="flex w-full flex-1 flex-col items-center
+            justify-center"
+          >
+            <div className="text-center text-6xl font-light text-coralRed sm:text-7xl">
+              Alpha Gallery
+            </div>
+            <div className="mt-4 text-begonia">
+              Explore and Upload Images just by a few clicks...
+            </div>
+            <Search containerClassName="mt-16" />
           </div>
-          <div className="mt-4 text-begonia">
-            Explore and Upload Images just by a few clicks...
-          </div>
-          <Search containerClassName="mt-16" />
+
+          <ScrollButton containerClassName="mb-4" />
         </div>
 
-        <ScrollButton containerClassName="mb-4" />
-      </div>
-
-      <div
-        className="flex min-h-screen w-screen flex-col items-center
+        <div
+          className="flex min-h-screen w-screen flex-col items-center
         justify-center bg-gradient-to-b from-chineseBlackVoid to-chineseBlack"
-      ></div>
-    </main>
+        ></div>
+      </main>
+    </>
   );
 }
