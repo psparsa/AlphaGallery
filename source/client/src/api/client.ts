1107362783a99eb7e6883241c32446e896f65467
PS-PARSA <57572461+psparsa@users.noreply.github.com>
2023-02-18 04:26:16 +0330

    chore: api address defined in an `env` file

--- source/client/src/api/client.ts
+++ source/client/src/api/client.ts
@@ -1,5 +1,8 @@
 import Axios from 'axios';
 
+// eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+const baseURL = `${process.env.NEXT_PUBLIC_API_PROTOCOL}://${process.env.NEXT_PUBLIC_API_HOSTNAME}:${process.env.NEXT_PUBLIC_API_PORT}/api`;
+
 export const Client = Axios.create({
-  baseURL: 'http://127.0.0.1:1337/api',
+  baseURL,
 });
