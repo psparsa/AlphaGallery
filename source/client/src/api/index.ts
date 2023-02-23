afb8d1a1a8a449804f49d827c6343dca73a9d40c
PS-PARSA <57572461+psparsa@users.noreply.github.com>
2023-02-23 23:44:27 +0330

    feat(`api/services`): `getUserInfo` module implemented to get user's information

--- source/client/src/api/index.ts
+++ source/client/src/api/index.ts
@@ -1,3 +1,4 @@
 export * from './services/get-posts';
 export * from './services/register-user';
 export * from './services/login';
+export * from './services/get-user-info';
--- source/client/src/api/services/login.ts
+++ source/client/src/api/services/login.ts
@@ -1,12 +1,20 @@
 import { Client } from '../client';
-import { UserInfoSchema, UserInfoResponse } from '@/api';
+import { z } from 'zod';
+import { UserInfoSchema } from '@/api';
 import { useMutation } from '@tanstack/react-query';
+
+export const LoginSchema = z.object({
+  jwt: z.string(),
+  user: UserInfoSchema,
+});
+
+export type LoginResponse = z.infer<typeof LoginSchema>;
 
 export const login = async ({
   identifier,
   password,
 }: Record<'identifier' | 'password', string>) => {
-  const { data } = await Client<UserInfoResponse>({
+  const { data } = await Client<LoginResponse>({
     method: 'POST',
     url: '/api/auth/local',
     data: {
