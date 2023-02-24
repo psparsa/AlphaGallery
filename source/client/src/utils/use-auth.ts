b17b785d89375e715c2b7ca0582fc33a0fc0c950
PS-PARSA <57572461+psparsa@users.noreply.github.com>
2023-02-24 22:53:07 +0330

    feat(`utils`): `useAuth` hook implemented for handeling authentication

---
+++ source/client/src/utils/use-auth.ts
@@ -0,0 +1,35 @@
+import React from 'react';
+import { UserInfoResponse, useUserInfo } from '@/api';
+import Cookies from 'js-cookie';
+
+export interface UseAuthArguments {
+  initialUserData?: UserInfoResponse;
+}
+
+export const useAuth = ({ initialUserData }: UseAuthArguments) => {
+  const [token, setToken] = React.useState(Cookies.get('token'));
+  const { data, isLoading } = useUserInfo({
+    jwt: token,
+    initialData: initialUserData,
+    onError: () => Cookies.remove('token'),
+  });
+  const userId = data?.id;
+
+  const userDataIsLoading = isLoading && !!token;
+  const isAuthenticated = !!userId && !!token;
+
+  React.useEffect(() => {
+    const updateToken = () => {
+      const currentToken = Cookies.get('token');
+      if (currentToken !== token) setToken(currentToken);
+    };
+
+    window.addEventListener('focus', updateToken);
+    return () => window.removeEventListener('focus', updateToken);
+  }, [token]);
+
+  return {
+    isAuthenticated,
+    userDataIsLoading,
+  };
+};
