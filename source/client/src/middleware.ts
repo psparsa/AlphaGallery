69131592f1e03c1ddb4643861c71a4f4b3209da9
PS-PARSA <57572461+psparsa@users.noreply.github.com>
2023-03-15 15:42:35 +0330

    perf: the `middleware` used for protecting routes

---
+++ source/client/src/middleware.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+const protectedPaths = ['/upload'];
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get('token')?.value;
+  const isProtectedPath = protectedPaths.some((path) =>
+    request.nextUrl.pathname.startsWith(path)
+  );
+  const isVisitingLoginPage = request.nextUrl.pathname.startsWith('/login');
+
+  if (isProtectedPath && !token)
+    return NextResponse.redirect(new URL('/', request.url));
+
+  if (isVisitingLoginPage && token)
+    return NextResponse.redirect(new URL('/', request.url));
+}
--- source/client/src/pages/login.tsx
+++ source/client/src/pages/login.tsx
@@ -1,6 +1,4 @@
 import 'react-toastify/dist/ReactToastify.css';
-import { getUserInfo } from '@/api';
-import type { GetServerSideProps } from 'next';
 import { LoginForm } from '@/components/common/login-form/login-form';
 import { Roboto } from '@next/font/google';
 import Head from 'next/head';
@@ -8,29 +6,6 @@ import React from 'react';
 import { ToastContainer } from 'react-toastify';
 
 const roboto = Roboto({ weight: ['300', '400'], subsets: ['latin'] });
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const token = context.req.cookies.token;
-
-  if (!token)
-    return {
-      props: {},
-    };
-
-  const { id: userId } = await getUserInfo({ jwt: token }).catch();
-
-  if (userId)
-    return {
-      redirect: {
-        destination: '/',
-        permanent: false,
-      },
-    };
-
-  return {
-    props: {},
-  };
-};
 
 const LoginPage = () => {
   return (
