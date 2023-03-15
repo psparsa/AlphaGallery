c597a2ef0f49c17935e9f46e9a34396ffa1bc947
PS-PARSA <57572461+psparsa@users.noreply.github.com>
2023-03-15 12:48:57 +0330

    feat(`components`): `Textarea` component

---
+++ source/client/src/components/common/textarea/textarea.tsx
@@ -0,0 +1,67 @@
+import React, {
+  ChangeEvent,
+  ChangeEventHandler,
+  FocusEventHandler,
+  LegacyRef,
+} from 'react';
+import { twMerge } from 'tailwind-merge';
+
+interface TextareaProperties {
+  containerClassName?: string;
+  inputRef?: LegacyRef<HTMLTextAreaElement>;
+  invalid?: boolean;
+  maxLength?: number;
+  onBlur?: FocusEventHandler<HTMLTextAreaElement>;
+  onChange?: ChangeEventHandler<HTMLTextAreaElement>;
+  placeHolder?: string;
+  value?: string;
+}
+
+export const Textarea = ({
+  maxLength,
+  containerClassName,
+  inputRef,
+  onBlur,
+  onChange,
+  value,
+  placeHolder,
+  invalid,
+}: TextareaProperties) => {
+  const DEFAULT_MAX_LENGTH = 200;
+  const DEFAULT_PLACEHOLDER = 'Enter something here...';
+
+  const [inputValue, setInputValue] = React.useState(value);
+  const length = inputValue?.length ?? 0;
+
+  const handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    setInputValue(event.target.value);
+    if (onChange) onChange(event);
+  };
+
+  React.useEffect(() => {
+    setInputValue(value);
+  }, [value]);
+
+  return (
+    <div
+      className={twMerge(
+        containerClassName,
+        'flex w-60  flex-col overflow-hidden rounded border-2 border-solid bg-snow',
+        invalid ? 'border-coralRed' : undefined
+      )}
+    >
+      <textarea
+        className="h-52 w-full resize-none p-2 outline-none"
+        maxLength={maxLength ?? DEFAULT_MAX_LENGTH}
+        ref={inputRef}
+        onBlur={onBlur}
+        onChange={handleChange}
+        value={inputValue}
+        placeholder={placeHolder ?? DEFAULT_PLACEHOLDER}
+      />
+      <div className="w-full pr-3 pb-2 text-right text-gunmetal">
+        {length}/{maxLength ?? DEFAULT_MAX_LENGTH}
+      </div>
+    </div>
+  );
+};
