8054ee748850912e0f4eea5b183b8a8d1176a523
PS-PARSA <57572461+psparsa@users.noreply.github.com>
2023-03-18 22:50:55 +0330

    test(`components/button`): unit test added

--- source/client/src/components/common/button/button.test.tsx
+++ source/client/src/components/common/button/button.test.tsx
@@ -1,10 +1,39 @@
 import { render, screen } from '@testing-library/react';
 import { Button } from '.';
+import userEvent from '@testing-library/user-event';
 
-test('Renders Button', async () => {
+test('checks button value', async () => {
   const LABEL = 'CLICK ON ME';
   render(<Button>{LABEL}</Button>);
 
   const buttonElement = await screen.findByText(LABEL);
   expect(buttonElement).toHaveTextContent(LABEL);
+});
+
+test('checks className value', async () => {
+  const LABEL = 'CLICK ON ME';
+  const containerClassName = 'test-class';
+  render(<Button containerClassName={containerClassName}>{LABEL}</Button>);
+
+  const buttonElement = await screen.findByText(LABEL);
+  expect(buttonElement).toHaveClass(containerClassName);
+});
+
+const addTagPToDom = (content: string) => {
+  const element = document.createElement('p');
+  Reflect.set(element, 'innerHTML', content);
+  document.body.append(element);
+};
+test('tests onClick event', async () => {
+  const user = userEvent.setup();
+  const content = 'GfFbF%@^';
+  render(
+    <Button onClick={() => addTagPToDom(content)}>Add a P tag to DOM</Button>
+  );
+
+  const buttonElement = await screen.findByRole('button');
+  await user.click(buttonElement);
+
+  const tagP = await screen.findByText(content);
+  expect(tagP).toHaveTextContent(content);
 });
