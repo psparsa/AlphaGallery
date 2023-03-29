e0ee925dd0a394f6db60ef1f3f1668d9ada5a381
PS-PARSA <57572461+psparsa@users.noreply.github.com>
2023-03-29 16:01:21 +0330

    test(`components/login-form`): test cases splitted into smaller cases

--- source/client/src/components/common/login-form/login-from.test.tsx
+++ source/client/src/components/common/login-form/login-from.test.tsx
@@ -47,10 +47,55 @@ test('LOGIN: tests form validation', async () => {
   });
 });
 
-test('REGISTER: tests form validation', async () => {
+test('REGISTER: tests invalid email', async () => {
   const { container } = render(<LoginForm />, { wrapper: Wrapper });
 
   fireEvent.click(screen.getByText('Register a new account'));
+
+  fireEvent.change(screen.getByPlaceholderText('Enter your Email'), {
+    target: { value: 'invalid-email.org' },
+  });
+
+  const registerBTNContainer = screen.getByTestId('register-button');
+  const registerBTN = registerBTNContainer.querySelector('button');
+
+  await waitFor(() => {
+    expect(registerBTN).toHaveAttribute('disabled', '');
+    expect(container).toHaveTextContent('Enter a valid email');
+  });
+});
+
+test('REGISTER: tests password quality', async () => {
+  const { container } = render(<LoginForm />, { wrapper: Wrapper });
+
+  fireEvent.change(screen.getByPlaceholderText('Enter your password'), {
+    target: { value: '123' },
+  });
+  await waitFor(() =>
+    expect(container).toHaveTextContent(
+      'Password must be at least 6 characters'
+    )
+  );
+
+  fireEvent.change(screen.getByPlaceholderText('Enter your password'), {
+    target: { value: '123456' },
+  });
+  await waitFor(() =>
+    expect(container).not.toHaveTextContent(
+      'Password must be at least 6 characters'
+    )
+  );
+
+  fireEvent.change(screen.getByPlaceholderText('Enter your password'), {
+    target: { value: 'x'.repeat(500) },
+  });
+  await waitFor(() =>
+    expect(container).toHaveTextContent('Password is too long!')
+  );
+});
+
+test('REGISTER: tests required inputs', async () => {
+  const { container } = render(<LoginForm />, { wrapper: Wrapper });
 
   const inputs = {
     email: screen.getByPlaceholderText('Enter your Email'),
@@ -80,27 +125,6 @@ test('REGISTER: tests form validation', async () => {
     expect(container).toHaveTextContent('Email is required');
     expect(container).toHaveTextContent('Username is required');
     expect(container).toHaveTextContent(
-      'Password must be at least 6 characters'
-    );
-  });
-
-  fireEvent.change(inputs.email, { target: { value: 'invalid-email.com' } });
-
-  await waitFor(() => {
-    expect(container).toHaveTextContent('Enter a valid email');
-  });
-
-  fireEvent.change(inputs.email, {
-    target: { value: 'valid.email@gmail.com' },
-  });
-  fireEvent.change(inputs.username, { target: { value: 'admin' } });
-  fireEvent.change(inputs.password, { target: { value: '123456' } });
-
-  await waitFor(() => {
-    expect(registerBTN).not.toHaveAttribute('disabled', '');
-    expect(container).not.toHaveTextContent('Email is required');
-    expect(container).not.toHaveTextContent('Username is required');
-    expect(container).not.toHaveTextContent(
       'Password must be at least 6 characters'
     );
   });
