73f818f38a7b0ee6f51b1a9741b124c48f61f282
PS-PARSA <57572461+psparsa@users.noreply.github.com>
2023-09-29 17:17:13 +0330

    feat(server): restore configurations on bootstrap

---
+++ source/server/config/plugins.ts
@@ -0,0 +1,11 @@
+export default () => ({
+  "config-sync": {
+    enabled: true,
+    config: {
+      syncDir: "config/sync/",
+      minify: false,
+      soft: false,
+      importOnBootstrap: true,
+    },
+  },
+});
